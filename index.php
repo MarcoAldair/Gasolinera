19bc4a97b8ace3144aa1d7fe0fc72dc8db77e9bb
MarcoAldair <76014782+MarcoAldair@users.noreply.github.com>
2021-06-23 14:55:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Inicio</title>
 	<link rel="stylesheet" href="style/bootstrap.css">
 </head>
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  		<div class="container-fluid">
-    		<span class="navbar-brand mb-0 h1">Gasolinera</span>
-		    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-	      		<div class="navbar-nav">
-			        <a class="nav-link active" aria-current="page" href="Empleado/view.php">Empleados</a>
-			        <a class="nav-link active" aria-current="page" href="#">Dispensador</a>
-			        <a class="nav-link active" aria-current="page" href="#">Ventas</a>
-		      	</div>
-		    </div>
+		<div class="container-fluid">
+    			<span class="navbar-brand mb-0 h1">Gasolinera</span>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div class="navbar-nav">
+					<a class="nav-link active" aria-current="page" href="Empleado/view.php">Empleados</a>
+					<a class="nav-link active" aria-current="page" href="#">Dispensador</a>
+					<a class="nav-link active" aria-current="page" href="#">Ventas</a>
+				</div>
+		    	</div>
   		</div>
 	</nav>
 </body>
